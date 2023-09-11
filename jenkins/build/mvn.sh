c035b8ff2733dc7204f450498b9ece66f644abdf
omareldeeeb <omar_kh_deeb@yahoo.com>
2023-09-11 19:17:18 -0400

    workspace edit

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,7 +4,7 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/pipeline
-#WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+#WORKSPACE=/home/jenkins/jenkins-data/pipeline
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/cicd
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
