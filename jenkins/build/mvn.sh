d0459aea0bb72c09dc809b2043c50b8a19b2f230
omareldeeeb <omar_kh_deeb@yahoo.com>
2023-09-16 08:59:45 -0400

    docker sock mapping

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -7,4 +7,4 @@ echo "***************************"
 #WORKSPACE=/home/jenkins/jenkins-data/pipeline
 WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/cicd
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -v /var/run/docker.sock:/var/run/docker.sock -w /app maven:3-alpine "$@"
